cd0ef10fd8f1aea09ddb5691b61633ed307241e5
flyweistTech123 <147515698+flyweistTech123@users.noreply.github.com>
2024-10-23 14:06:23 +0530

    updated

---
+++ src/pages/Contact-Us/ContactForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const ContactForm = () => {
+    return (
+        <>
+            <div className='contact1'>
+                <div className='contact2'>
+                    <div className='contact3'>
+                        <div className='contact4'>
+                            <h2>Have Any Questions For Us ?</h2>
+                            <p>Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like. </p>
+
+                        </div>
+                        <div className='contact5'>
+                            <div className='ccontact6'>
+                                <div className='contact7'>
+                                    <input type="text" placeholder="Your Name*"/>
+                                </div>
+                            </div>
+                            <div className='contact6'>
+                                <div className='contact7'>
+                                    <input type="text" placeholder="Your Email*"/>
+                                </div>
+                            </div>
+                            <div className='contact6'>
+                                <div className='contact7'>
+                                    <input type="text" placeholder="Your Subject*"/>
+                                </div>
+                            </div>
+                            <div className='contact6'>
+                                <div className='contact7'>
+                                   <textarea name="" id="" placeholder='Your Message*'></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ContactForm
--- src/pages/Contact-Us/ContactUs.jsx
+++ src/pages/Contact-Us/ContactUs.jsx
@@ -3,15 +3,19 @@ import PagesLayout from '../../components/CommonComponent/PagesLayout'
 import './ContactUs.css'
 import MyNavbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+import ContactForm from './ContactForm'
 
 const ContactUs = () => {
     return (
         <>
             <MyNavbar />
             <div className='aboutus'>
-                <PagesLayout name={"Contact Us"}/>
+                <PagesLayout name={"Contact Us"} />
             </div>
-            <Footer/>
+            <div className='privacy-policy'>
+                <ContactForm />
+            </div>
+            <Footer />
         </>
     )
 }
