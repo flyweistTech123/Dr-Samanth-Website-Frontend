47bde60cc14654d10113aa2df4341f642c8f6592
flyweistTech123 <147515698+flyweistTech123@users.noreply.github.com>
2024-10-25 19:24:27 +0530

    updated

--- src/pages/Activity/RightSideSection.jsx
+++ src/pages/Activity/RightSideSection.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { RiVerifiedBadgeFill } from "react-icons/ri";
+
+
 
 const RightSideSection = () => {
     return (
@@ -16,11 +19,44 @@ const RightSideSection = () => {
                     <div className='rightsidesection2'>
                         <span>Latest Activities</span>
                     </div>
-                    <div className='rightsidesection3'>
-                        <p>There are no recently active members</p>
+                    <div className='rightsidesection4'>
+                        <ul>
+                            <li>
+                                <img loading="lazy" decoding="async" src="https://wordpress.iqonic.design/product/wp/socialv/wp-content/uploads/avatars/1/1696595070-bpthumb.jpg" class="d-block rounded-circle user-1-avatar avatar-50 photo" width="50" height="50" alt="Profile Photo" />
+                                <div className='rightsidesection5'>
+                                    <div className='rightsidesection6'>
+                                        <h4>
+                                            Jenny Wilson
+                                            <RiVerifiedBadgeFill color='#3897F0' />
+                                        </h4>
+                                        <span>joined the group</span>
+                                        <h4>Ultimate Nerds</h4>
+                                    </div>
+                                    <span>3 months ago</span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='rightsidesection4'>
+                        <ul>
+                            <li>
+                                <img loading="lazy" decoding="async" src="https://wordpress.iqonic.design/product/wp/socialv/wp-content/uploads/avatars/1/1696595070-bpthumb.jpg" class="d-block rounded-circle user-1-avatar avatar-50 photo" width="50" height="50" alt="Profile Photo" />
+                                <div className='rightsidesection5'>
+                                    <div className='rightsidesection6'>
+                                        <h4>
+                                            Jenny Wilson
+                                            <RiVerifiedBadgeFill color='#3897F0' />
+                                        </h4>
+                                        <span>posted an update</span>
+                                        {/* <h4>Ultimate Nerds</h4> */}
+                                    </div>
+                                    <span>a year ago</span>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
